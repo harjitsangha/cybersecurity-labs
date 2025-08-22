30d2d26d8d5c68e18abbbe6c9b189fc5e7236d48
Harjit <harjitsangha41@outlook.com>
2025-08-21 17:32:58 -0700

    added variables_dataTypes.py

---
+++ variables_dataTypes.py
@@ -0,0 +1,26 @@
+# Variables
+
+# Strings
+name = "Harjit"
+
+# Integers
+age = 25
+
+# Float
+price = 10.99
+gpa = 3.2
+
+# Boolean
+is_hacker = True
+
+# Lists & Disctionaries
+skills = ["Python", "Networking", "Blockchain"]
+profile = {"name": name, "age": age, "skills": skills}
+
+print(f"Hello {name}")
+print(profile)
+
+if is_hacker:
+    print(f"Hacker: {is_hacker}")
+else:
+    print("Not a hacker")
