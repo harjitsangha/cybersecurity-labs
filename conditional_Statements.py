1d2de8b0091c635507cadb42c7b2baeccc521966
Harjit <harjitsangha41@outlook.com>
2025-08-24 13:35:36 -0700

    updated conditional_Statements.py

--- conditional_Statements.py
+++ conditional_Statements.py
@@ -16,3 +16,18 @@ if response == "Y":
     print("Have some food!")
 else:
     print("No food for you!")
+
+# conditional expressions: one line shortcut for the if-else statement (ternary operator)
+# X if condition else Y
+
+num = 5
+a = 6
+b = 7
+
+print("Positive" if num > 0 else "Negative")
+result = "EVEN" if num % 2 == 0 else "ODD"
+print(result)
+
+max_num = a if a > b else b
+min_num = a if a < b else b
+print(max_num)
