d3930f7ced85e6df31098eb1e25f74f88b0ea705
Harjit <harjitsangha41@outlook.com>
2025-08-26 10:32:41 -0700

    updated variables_dataTypes.py

--- python/variables_dataTypes.py
+++ python/variables_dataTypes.py
@@ -71,4 +71,26 @@ for grocery in groceries:
         print(item, end =" ")
     print()
 
+# dictionary = collection of {key:value} pairs ordered and changeable. No duplicates.
 
+capitals = {"USA": "Washington D.C",
+            "India": "New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+print(capitals.get("USA"))
+capitals.update({"Germany": "Berlin"})
+capitals.pop("China")
+capitals.popitem()
+keys = capitals.keys()
+values = capitals.values()
+
+for key in capitals.keys():
+    print(key)
+
+for value in capitals.values():
+    print(value)
+
+items = capitals.items()
+for key,value in capitals.items():
+    print(f"{key}: {value}")
