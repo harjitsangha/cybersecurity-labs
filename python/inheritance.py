39c79ca6e72e9ffbf0c241d33b62c4ca347a4f4b
Harjit <harjitsangha41@outlook.com>
2025-09-05 17:33:59 -0700

    added inheritance.py

---
+++ python/inheritance.py
@@ -0,0 +1,35 @@
+# inheritance = allows a class to inherit attributes and methods from another class
+#               helps with code reusability and extensibility
+#               class Child(Parent)
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+    
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+    
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
+
+class Mouse(Animal):
+    def speak(self):
+        print("SQUEEk!")
+
+dog = Dog("Scooby")
+cat = Cat("Garfield")
+mouse = Mouse("Mickey")
+
+
+dog.eat()
+dog.sleep()
+cat.speak()
